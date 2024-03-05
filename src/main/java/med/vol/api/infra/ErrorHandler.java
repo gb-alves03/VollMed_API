6288f1efde5e329fef27683d65204c78a780b6a9
gbalves03 <gabrielhenriquealves321@gmail.com>
2024-03-05 17:39:37 -0300

    Creation of a class to isolatte exceptions handling

---
+++ src/main/java/med/vol/api/infra/ErrorHandler.java
@@ -0,0 +1,30 @@
+package med.vol.api.infra;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.apache.coyote.BadRequestException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ErrorHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity handle404Error() {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity handle400Error(MethodArgumentNotValidException exception) {
+        var errors = exception.getFieldErrors();
+        return ResponseEntity.badRequest().body(errors.stream().map(DataErrorValidation::new).toList());
+    }
+
+    private record DataErrorValidation(String field, String message) {
+        public DataErrorValidation(FieldError error) {
+            this(error.getField(), error.getDefaultMessage());
+        }
+    }
+}
