c18212b2eec391d31caf277c1dc13be07de5ec5f
gbalves03 <gabrielhenriquealves321@gmail.com>
2024-03-20 23:19:20 -0300

    Implementation of Authentication and JWT to the application

---
+++ src/main/java/med/vol/api/controller/AutenticacaoController.java
@@ -0,0 +1,37 @@
+package med.vol.api.controller;
+
+import jakarta.validation.Valid;
+import med.vol.api.domain.usuario.DadosAutenticacao;
+import med.vol.api.domain.usuario.Usuario;
+import med.vol.api.infra.security.TokenService;
+import med.vol.api.infra.security.DadosTokenJWT;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+public class AutenticacaoController {
+
+    @Autowired
+    private AuthenticationManager manager;
+
+
+    @Autowired
+    private TokenService tokenService;
+
+    @PostMapping
+    public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados) {
+        var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
+        var authentication = manager.authenticate(token);
+
+        var tokenJWT = tokenService.gerarToken((Usuario) authentication.getPrincipal());
+
+        return ResponseEntity.ok(new DadosTokenJWT(tokenJWT));
+    }
+}
---
+++ src/main/java/med/vol/api/infra/security/DadosTokenJWT.java
@@ -0,0 +1,5 @@
+package med.vol.api.infra.security;
+
+public record DadosTokenJWT(String token) {
+
+}
